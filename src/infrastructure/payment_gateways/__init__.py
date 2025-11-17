ccf913944f0cf546ff04d3ad69397ee5237f5c66
agosset15 <ag@agosset.ru>
2025-11-17 18:03:49 +0300

    implemented yoomoney payment gateway

--- src/infrastructure/payment_gateways/__init__.py
+++ src/infrastructure/payment_gateways/__init__.py
@@ -1,10 +1,12 @@
 from .base import BasePaymentGateway, PaymentGatewayFactory
 from .telegram_stars import TelegramStarsGateway
 from .yookassa import YookassaGateway
+from .yoomoney import YoomoneyGateway
 
 __all__ = [
     "BasePaymentGateway",
     "PaymentGatewayFactory",
     "TelegramStarsGateway",
     "YookassaGateway",
+    "YoomoneyGateway"
 ]
